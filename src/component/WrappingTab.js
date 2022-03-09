699c6f7124b13372791d6e6e0bbebb89016f8739
akyo8 <imirisa8@gmail.com>
2022-03-09 09:57:05 -0500

    fix minor issue

--- src/component/WrappingTab.js
+++ src/component/WrappingTab.js
@@ -11,13 +11,13 @@ function WrappingTabs() {
     <div>
         <Collapse defaultActiveKey={['1']} ghost>
             <Panel header="This is panel header 1" key="1">
-            <p>{text}</p>
+                <p>{text}</p>
             </Panel>
             <Panel header="This is panel header 2" key="2">
-            <p>{text}</p>
+                <p>{text}</p>
             </Panel>
             <Panel header="This is panel header 3" key="3">
-            <p>{text}</p>
+                <p>{text}</p>
             </Panel>
         </Collapse>
     </div>
