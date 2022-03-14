cbfcc20fe8656bde02b25e27621c503a9a2c3cff
akyo8 <imirisa8@gmail.com>
2022-03-14 12:48:42 -0400

    update UI for wrap

--- src/component/WrappingTab.js
+++ src/component/WrappingTab.js
@@ -1,28 +1,15 @@
-import React from 'react';  
+import React from 'react';
 import { Collapse } from 'antd';
+import DetailedItem from './DetailedItem/DetailedItem';
 function WrappingTabs() {
   const { Panel } = Collapse;
-  const text = `
-    A dog is a type of domesticated animal.
-    Known for its loyalty and faithfulness,
-    it can be found as a welcome guest in many households across the world.
-    `;
+  console.log(Panel);
   return (
     <div>
-        <Collapse defaultActiveKey={['1']} ghost>
-            <Panel header="This is panel header 1" key="1">
-                <p>{text}</p>
-            </Panel>
-            <Panel header="This is panel header 2" key="2">
-                <p>{text}</p>
-            </Panel>
-            <Panel header="This is panel header 3" key="3">
-                <p>{text}</p>
-            </Panel>
-        </Collapse>
+      <DetailedItem />
+      <DetailedItem />
     </div>
   );
 }
 
 export default WrappingTabs;
-
