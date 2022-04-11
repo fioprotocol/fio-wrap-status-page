4d98330f695baad4ecb4b752a01422e98d8f161c
akyo8 <imirisa8@gmail.com>
2022-04-10 23:16:13 -0400

    config web3

--- src/component/UnwrappingTab.js
+++ src/component/UnwrappingTab.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Collapse } from 'antd';
 import DetailedItem from './DetailedItem/DetailedItem';
-import {INFURA_URL} from '../config/config-testnet';
-import Web3 from 'web3';
+import web3 from '../config/config'
 function UnwrappingTabs() {
-  const { Panel } = Collapse;
+  console.log(web3);
     // const buttonClick = (e) => {
     //     console.log("button clicked");
     // };
-  const web3 = new Web3(INFURA_URL);
-  console.log(INFURA_URL);
   return (
     <div>
       <DetailedItem />
--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,5 @@
-import conf_testnet from "./config-testnet";
-import {INFURA_URL} from '../config/config-testnet';
+import { INFURA_URL } from "./config-testnet";
 import Web3 from "web3";
 
-let config = conf_testnet;
 let web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URL))
-
-console.log(web3);
-export default {
-  ...config,
-  web3,
-};
+export default web3;
